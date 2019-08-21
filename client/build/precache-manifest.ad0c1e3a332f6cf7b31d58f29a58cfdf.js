d5efe1f4a27c89c8a5026160cd561356fa4965ca
blinkvondom <blynk.rankin@gmail.com>
2019-08-21 10:17:45 -0600

    build folder

---
+++ client/build/precache-manifest.ad0c1e3a332f6cf7b31d58f29a58cfdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab6a210da24f9ed245a2cd82dff4799",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ade60e7c6762303d4a1f",
+    "url": "/static/css/main.d01b7dcc.chunk.css"
+  },
+  {
+    "revision": "ed4e6d275d1422c2fe62",
+    "url": "/static/js/2.9cefb664.chunk.js"
+  },
+  {
+    "revision": "ade60e7c6762303d4a1f",
+    "url": "/static/js/main.0614a87d.chunk.js"
+  },
+  {
+    "revision": "db6be01aac7cb1c6d4ff",
+    "url": "/static/js/runtime~main.0033641b.js"
+  }
+]);
